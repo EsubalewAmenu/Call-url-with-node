cfe1b25ca99d3bbcb2637d8a810cf7c7690f2f3c
Esubalew Desktop <esubalew.a2009@gmail.com>
2022-04-05 12:03:02 +0300

    first commit

---
+++ tezt.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+const get = async () => {
+    axios.get('https://dashencon.com/tezt/wp-json/ds_bt/v1/trade/spot').then(res => {
+        console.log(res.data);
+    })
+}
+setInterval(get, 4000)
